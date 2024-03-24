c80c33ff41baa1ffd947e93336818989ad69f2a4
rraagg <rob.a.gallagher@gmail.com>
2024-03-24 12:55:09 -0700

    swapping hard coded values for variables

--- calculator/engine_test.go
+++ calculator/engine_test.go
@@ -9,16 +9,18 @@ import (
 func TestAdd(t *testing.T) {
 	// Arrange
 	e := calculator.Engine{}
+	x, y := 2.5, 3.5
+	want := 6.0
 	// Act
-	got := e.Add(2.5, 3.5)
+	got := e.Add(x, y)
 	// Asser
 	if got != 6.0 {
 		t.Errorf(
 			"Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f",
-			2.5,
-			3.5,
+			x,
+			y,
 			got,
-			6.0,
+			want,
 		)
 	}
 }
