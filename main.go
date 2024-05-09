29bcfcae80d8cff1e68e0271f3a87cb256128f0e
rraagg <rob.a.gallagher@gmail.com>
2024-05-08 20:57:20 -0700

    cleanup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/rraagg/go-tdd/calculator"
+	"github.com/rraagg/go-tdd/input"
+)
 
 func main() {
-	fmt.Println("Doing go TDD")
+	expr := flag.String("expression", "", "mathematical expression to parse")
+	flag.Parse()
+
+	engine := calculator.NewEngine()
+	validator := input.NewValidator(engine.GetNumOperands(), engine.GetValidOperators())
+	parser := input.NewParser(engine, validator)
+	result, err := parser.ProcessExpression(*expr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(*result)
 }
